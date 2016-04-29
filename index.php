1ef3c8901fbf973f79c4fb00d949c8f00906e416
Samuel Garcia <samg15wr@gmail.com>
2016-04-29 17:34:45 +0200

    Première version du squelette

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <!DOCTYPE html>
-<?php
-  $db = new PDO("pgsql:host=localhost;dbname=sciencehub", "postgres", "samsam");
-
-  $qry = $db->prepare("SELECT nomeq FROM equipes;");
-  $qry->execute();
-
-?>
 <html lang="fr">
   <head>
     <meta charset="utf-8">
@@ -14,7 +7,7 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>ScienceHUB : Plateforme collaborative de recherche</title>
 
-    <!-- Bootstrap -->
+    <!-- Bootstrap-->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -23,18 +16,48 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <link href="css/squelette.css" rel="stylesheet">
   </head>
   <body>
-    <header>
-      <h1>Science HUB biatch <br><small>Plateforme collaborative de recherche</small></h1>
-    </header>
-    <?php
-      while ($donnees = $qry->fetch()) {
-        echo $donnees['nomeq'];
-      }
+    <div id="wrap-container">
+      <header>
+        <a href="#"><img id="logo" src="images/logo/logo-transparent-nom.png"/></a>
+        <fieldset id="fieldset-header" >
+          <legend>Bonjour Machin</legend>
+          <a href="#" class="btn-fieldset btn btn-primary">Mon profil</a>
+          <a href="#" class="btn-fieldset btn btn-danger">Déconnexion</a>
+        </fieldset>
+      </header>
+      <nav>
+        <center>
+        <ul id="wrap-li">
+          <li><a href="#" class="actif">Accueil</a></li>
+          <li><a href="#" ><strong>Présentation</strong></a></li>
+          <li><a href="#"><strong>Publications</strong></a></li>
+          <li><a href="#"><strong>Evénements</strong></a></li>
+          <li><a href="#"><strong>Messages</strong></a></li>
+          <li><a href="#"><strong>Annuaire</strong></a></li>
+          <li><a href="#"><strong>Budget</strong></a></li>
+        </ul>
+        </center>
+      </nav>
+      <div id="wrap-content">
+        <div id="left-panel">
+          <div class="left-sub-panel">
 
-      $qry->closeCursor();
-    ?>
+          </div>
+          <div class="left-sub-panel">
+
+          </div>
+        </div>
+        <div id="right-panel">
+
+        </div>
+      </div>
+
+
+    </div>
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
