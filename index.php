99b5569f7addf69d8c565d9b3dac63f793f88f8c
Samuel Garcia <samg15wr@gmail.com>
2016-05-02 15:30:12 +0200

    Modification de index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <title>ScienceHUB : Plateforme collaborative de recherche</title>
 
     <!-- Bootstrap-->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -16,7 +16,8 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link href="/css/squelette.css" rel="stylesheet">
+    <link href="css/squelette.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
   </head>
   <body>
     <div id="wrap-container">
@@ -44,18 +45,56 @@
       <div id="wrap-content">
         <div id="left-panel">
           <div class="left-sub-panel">
-
+            <a href="#" class="inside-panel-link"><h2 class="inside-panel">Texte H2</h2></a>
+            <p class="inside-panel horodatage"><i>publié le 17.03.16 à 14h00</i></p>
+            <p class="panel-text">
+              Accenderat super his incitatum propositum ad nocendum aliqua mulier vilis,
+              quae ad palatium ut poposcerat intromissa insidias ei latenter obtendi prodiderat a
+              militibus obscurissimis. quam Constantina exultans ut in tuto iam locata mariti salute
+              muneratam vehiculoque inpositam per regiae ianuas emisit in publicum, ut his inlecebris
+              alios quoque ad indicanda proliceret paria vel maiora.
+            </p>
+            <a href="#" class="inside-panel">Lire plus</a>
           </div>
           <div class="left-sub-panel">
-
+            <a href="#" class="inside-panel-link"><h2 class="inside-panel">Texte H2</h2></a>
+            <p class="inside-panel horodatage"><i>publié le 17.03.16 à 14h00</i></p>
+            <p class="panel-text">
+              Accenderat super his incitatum propositum ad nocendum aliqua mulier vilis,
+              quae ad palatium ut poposcerat intromissa insidias ei latenter obtendi prodiderat a
+              militibus obscurissimis. quam Constantina exultans ut in tuto iam locata mariti salute
+              muneratam vehiculoque inpositam per regiae ianuas emisit in publicum, ut his inlecebris
+              alios quoque ad indicanda proliceret paria vel maiora.
+            </p>
+            <a href="#" class="inside-panel">Lire plus</a>
           </div>
+          <div class="left-sub-panel" style="margin-bottom:20px;">
+            <a href="#" class="inside-panel-link"><h2 class="inside-panel">Texte H2</h2></a>
+            <p class="inside-panel horodatage"><i>publié le 17.03.16 à 14h00</i></p>
+            <p class="panel-text">
+              Accenderat super his incitatum propositum ad nocendum aliqua mulier vilis,
+              quae ad palatium ut poposcerat intromissa insidias ei latenter obtendi prodiderat a
+              militibus obscurissimis. quam Constantina exultans ut in tuto iam locata mariti salute
+              muneratam vehiculoque inpositam per regiae ianuas emisit in publicum, ut his inlecebris
+              alios quoque ad indicanda proliceret paria vel maiora.
+            </p>
+            <a href="#" class="inside-panel">Lire plus</a>
+          </div>
+          <a href="publications.php" style="float:right;" class="btn btn-warning"><u><b>Voir plus</b></u></a>
         </div>
         <div id="right-panel">
+          <div id="newmessages" >
+            <h3 class="right-side-h3">Vos messages</h3>
+          </div>
+          <div id="calendrier">
+            <h3 class="right-side-h3">Calendrier</h3>
+          </div>
 
         </div>
       </div>
+      <footer>
 
-
+      </footer>
     </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
--- pages/connexion.php
+++ pages/connexion.php
@@ -8,7 +8,7 @@
     <title>ScienceHUB : Plateforme collaborative de recherche</title>
 
     <!-- Bootstrap-->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -16,8 +16,8 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link href="/css/squelette.css" rel="stylesheet">
-    <link href="/css/connexion.css" rel="stylesheet">
+    <link href="../css/squelette.css" rel="stylesheet">
+    <link href="../css/connexion.css" rel="stylesheet">
   </head>
   <body>
     <div id="wrap-container">
@@ -25,8 +25,7 @@
         <center><a href="#"><img id="logo" src="../images/logo/logo-transparent-nom.png"/></a></center>
       </header>
       <div id="wrap-content">
-
-        <form id="form-cnx" method="post" action="/index.php">
+        <form id="form-cnx" method="post" action="../index.php">
           <h1>Connectez-vous</h1>
           <table>
             <tr>
