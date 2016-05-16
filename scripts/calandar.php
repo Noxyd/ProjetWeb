6d990de1b42d1907b64e7a3162674772685d000c
Samuel Garcia <samg15wr@gmail.com>
2016-05-16 23:24:20 +0200

    Modification du style du calandrier + ajout des commentaires php

--- scripts/calandar.php
+++ scripts/calandar.php
@@ -6,6 +6,8 @@
      */
     function calculateDays($calculateMonth){
         //Initialisation des variables en fonction du mois choisi
+        // $firstDay est le premiers jour du mois de 0 à 6 (lundi correspond à 0)
+        // $numberOfDays est le nombre de jours dans le mois
         switch ($calculateMonth) {
             case 'avril':
                 $firstDay = 4;
@@ -18,7 +20,9 @@
                 break;
 
             default:
-                return "Error";
+                //Erreur
+                $firstDay = 0;
+                $numberOfDays = 1;
                 break;
         }
         //Les autres variables
@@ -28,28 +32,44 @@
         $countHint = 0; //Boucle de 0 à 6 (les jours de la semaine)
         $stop = 0;
         //Début du programme
+        //Initialisation des jours ne faisant pas parties du mois
         while ($countHint < $firstDay) {
+            //On place un espace dans chacunes des cases du tableau final $month
             $month[$row][$days[$countHint]] = " ";
+            //On incrémente l'indice permettant de se déplacer entre les jours
             $countHint += 1;
+            //Lorsque l'on atteint le jour précédent le premier jour du mois
+            //On sort de la boucle d'initialisation
         }
+        //On détermine la date correspondat au jour de la semaine
+        //Exemple : en le premier jour de mai 2016 est un dimanche
         while ($stop != 1) {
+            //Trace de la première itération, exemple avec le mois de Mai 2016:
+            //$row=0 || $countHint=6 || $days[6]='dimanche' || $countDays=1
             $month[$row][$days[$countHint]] = $countDays;
+            //$month[0]['dimanche']=1
+            //On incrémente ensuite les différents compteurs
             $countDays += 1;
             $countHint += 1;
+            //Si l'on dépasse le dimanche avec $countHint, on reprend le compte à 0
             if ($countHint === 7) {
                 $countHint = 0;
+                //On change également de ligne au niveau du calandrier
                 $row += 1;
             }
+            //Si l'on a placé toutes les dates dans le calandrier, on place des caractères
+            //'espace' dans le reste des jours de la semaine
             if($countDays === $numberOfDays+1){
                 while ($countHint != 7){
                     $month[$row][$days[$countHint]] = " ";
                     $countHint +=1;
                 }
+                //Lorsque le calandrier est plein, on place le flag de stop à 1 pour sortir de la boucle
                 $stop = 1;
             }
         }
-
-        echo '<table style="width:270px; text-align:center;margin:auto">';
+        //Le reste est l'affichage du calandrier sous forme de tableau
+        echo '<table style="width:270px; text-align:center;margin:auto" class="table table-striped">';
         echo "\n\t<tr>";
         echo "\n\t<center>";
         echo "\n\t\t<th>lun</th>";
@@ -61,13 +81,17 @@
         echo "\n\t\t<th>dim</th>";
         echo "\n\t</center>";
         echo "\n\t</tr>";
+        //Incrémentation des lignes ($rows)
         for ($i=0; $i <=$row; $i++) {
             echo "\n\t<tr>";
+            //Incrémentation des jours ($days)
             for ($j=0; $j < 7; $j++) {
+                //Affichage du contenu de $month, le tableau résultant
                 echo "\n\t\t<td>".$month[$i][$days[$j]]."</td>";
             }
             echo "\n\t</tr>";
         }
         echo "\n</table>\n";
     }
+    //FIN DE LA PROCEDURE
 ?>
